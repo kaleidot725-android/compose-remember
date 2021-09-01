528ad5660b7f8e32f241f480c0333d93f98abc48
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-09-01 21:30:47 +0900

    feat: update remember sample

--- app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -15,91 +15,17 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class MainActivity : ComponentActivity() {
-    val mainState = MainState()
-
+    private val mainState = MainState()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val count by mainState.count.collectAsState()
-
             SampleTheme {
                 if (10 < count) {
                     CounterOver10(count, { mainState.plus() }, { mainState.minus() })
                 } else {
-                    Counter(count, { mainState.plus() }, { mainState.minus() })
+                    CounterLessThan10(count, { mainState.plus() }, { mainState.minus() })
                 }
-            }
-        }
-    }
-}
-
-@Composable
-private fun CounterOver10(count: Int, onPlus: () -> Unit, onMinus: () -> Unit) {
-    var plusCount by remember { mutableStateOf(0) }
-    var minusCount by remember { mutableStateOf(0) }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .wrapContentHeight()
-                .fillMaxWidth()
-                .align(Alignment.Center)
-        ) {
-            Text(text = "Counter Over10", fontSize = 20.sp)
-            Text(text = "Count $count PlusCount $plusCount MinusCount $minusCount")
-            Button(
-                onClick = {
-                    plusCount++
-                    onPlus.invoke()
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "Plus")
-            }
-            Button(
-                onClick = {
-                    minusCount++
-                    onMinus.invoke()
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "Minus")
-            }
-        }
-    }
-}
-
-@Composable
-private fun Counter(count: Int, onPlus: () -> Unit, onMinus: () -> Unit) {
-    var plusCount by remember { mutableStateOf(0) }
-    var minusCount by remember { mutableStateOf(0) }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .wrapContentHeight()
-                .fillMaxWidth()
-                .align(Alignment.Center)
-        ) {
-            Text(text = "Counter", fontSize = 20.sp)
-            Text(text = "Count $count PlusCount $plusCount MinusCount $minusCount")
-            Button(
-                onClick = {
-                    plusCount++
-                    onPlus.invoke()
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "Plus")
-            }
-            Button(
-                onClick = {
-                    minusCount++
-                    onMinus.invoke()
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "Minus")
             }
         }
     }
@@ -115,5 +41,58 @@ class MainState {
 
     fun minus() {
         _count.value = _count.value - 1
+    }
+}
+
+@Composable
+private fun CounterOver10(count: Int, onPlus: () -> Unit, onMinus: () -> Unit) {
+    val plusCount = remember { mutableStateOf(0) }
+    val minusCount = remember { mutableStateOf(0) }
+    Counter(count, plusCount, minusCount, onPlus, onMinus)
+}
+
+@Composable
+private fun CounterLessThan10(count: Int, onPlus: () -> Unit, onMinus: () -> Unit) {
+    val plusCount = remember { mutableStateOf(0) }
+    val minusCount = remember { mutableStateOf(0) }
+    Counter(count, plusCount, minusCount, onPlus, onMinus)
+}
+
+@Composable
+private fun Counter(
+    count: Int,
+    plusCount: MutableState<Int>,
+    minusCount:  MutableState<Int>,
+    onPlus: () -> Unit,
+    onMinus: () -> Unit
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .align(Alignment.Center)
+        ) {
+            Text(text = "Counter", fontSize = 20.sp)
+            Text(text = "Count $count PlusCount $plusCount MinusCount $minusCount")
+            Button(
+                onClick = {
+                    plusCount.value = plusCount.value++
+                    onPlus.invoke()
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Plus")
+            }
+            Button(
+                onClick = {
+                    minusCount.value = plusCount.value++
+                    onMinus.invoke()
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Minus")
+            }
+        }
     }
 }
